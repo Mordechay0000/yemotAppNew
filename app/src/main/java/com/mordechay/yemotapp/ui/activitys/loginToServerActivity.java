1c799015cb6ff743488a9a0527fac414ad7b6462
Mordechay <m8572998@gmail.com>
2023-02-21 01:20:58 +0200

    add progress login and add error login message

--- app/src/main/java/com/mordechay/yemotapp/ui/activitys/loginToServerActivity.java
+++ app/src/main/java/com/mordechay/yemotapp/ui/activitys/loginToServerActivity.java
@@ -1,14 +1,15 @@
 package com.mordechay.yemotapp.ui.activitys;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,6 +20,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.mordechay.yemotapp.R;
 import com.mordechay.yemotapp.data.DataTransfer;
 
+import java.util.Objects;
+
 
 public class loginToServerActivity extends AppCompatActivity {
 
@@ -26,6 +29,7 @@ public class loginToServerActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText passwordEditText;
     private Button loginButton;
+    private ProgressBar prgLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,7 @@ public class loginToServerActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
+        prgLogin = findViewById(R.id.login_to_server_progress);
 
         loginButton.setOnClickListener(v -> {
             String email = emailEditText.getText().toString();
@@ -48,6 +53,8 @@ public class loginToServerActivity extends AppCompatActivity {
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "אנא מלא את כל השדות", Toast.LENGTH_SHORT).show();
             } else {
+                loginButton.setVisibility(View.GONE);
+                prgLogin.setVisibility(View.VISIBLE);
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
@@ -55,40 +62,27 @@ public class loginToServerActivity extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
+                                    if (user != null) {
+                                        DataTransfer.setUsername(user.getEmail());
+                                    }else{
+                                        Toast.makeText(loginToServerActivity.this, "שגיאה חמורה בהתחברות לאפליקציה.", Toast.LENGTH_LONG).show();
+                                        finish();
+                                    }
+                                    DataTransfer.setUid(user.getUid());
+                                    Intent intent = new Intent(loginToServerActivity.this, LoginActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    // TODO: test is task result Returns as expected
-                                    Toast.makeText(loginToServerActivity.this, "האימות נכשל. הסיבה:  " + task.getResult(),
+                                    Toast.makeText(loginToServerActivity.this, "האימות נכשל. הסיבה:  " + Objects.requireNonNull(task.getException()).getLocalizedMessage(),
                                             Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
+                                    loginButton.setVisibility(View.VISIBLE);
+                                    prgLogin.setVisibility(View.GONE);
                                 }
                             }
                         });
             }
         });
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            DataTransfer.setUsername(currentUser.getEmail());
-            DataTransfer.setUid(currentUser.getUid());
-            startActivity(new Intent(loginToServerActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-        }
-    }
-
-
-
-
-    private void updateUI(FirebaseUser currentUser) {
-        if(currentUser != null){
-            DataTransfer.setUsername(currentUser.getEmail());
-            DataTransfer.setUid(currentUser.getUid());
-            startActivity(new Intent(loginToServerActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-        }
     }
 
 
