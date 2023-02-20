021bf1067a83960babecea733f502452b6abf7c9
Mordechay <m8572998@gmail.com>
2023-02-20 15:53:09 +0200

    add open screen (open activity) and change launcher to new activity (open activity)

---
+++ app/src/main/java/com/mordechay/yemotapp/ui/activitys/openActivity.java
@@ -0,0 +1,16 @@
+package com.mordechay.yemotapp.ui.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mordechay.yemotapp.R;
+
+public class openActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+    }
+}
