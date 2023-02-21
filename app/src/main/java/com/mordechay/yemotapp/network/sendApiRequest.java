88688a99a64a1a2ed761f609319205978f0e8c44
Mordechay <m8572998@gmail.com>
2023-02-21 21:15:23 +0200

    disable verify login

--- app/src/main/java/com/mordechay/yemotapp/network/sendApiRequest.java
+++ app/src/main/java/com/mordechay/yemotapp/network/sendApiRequest.java
@@ -1,8 +1,6 @@
 package com.mordechay.yemotapp.network;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,47 +10,18 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.auth.FirebaseAuth;
-import com.mordechay.yemotapp.ui.activitys.LoginActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Objects;
-
-public class sendApiRequest implements testIsExitsUser.RespondsListener{
-    private String  type;
+public class sendApiRequest{
+    private final String  type;
     private final Activity act;
     private final RespondsListener respondsListener;
 
     private final String networkurl;
-    ProgressDialog progressDialog;
     private JSONObject jsonObject = null;
-    private FirebaseAuth mAuth;
-    private String responseString;
 
-    @Override
-    public void onSuccess(String result) {
-        if(result.equals("ok")){
-            respondsListener.onSuccess(this.responseString, this.type);
-        }else if (result.equals("Error: Account blocked")) {
-            Toast.makeText(act, "המשתמש נחסם.", Toast.LENGTH_SHORT).show();
-
-            mAuth.signOut();
-            act.startActivity(new Intent(act, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-
-
-        }else{
-            Toast.makeText(act, "המשתמש לא רשום, זה אומר שהאפליקציה גנובה!!!", Toast.LENGTH_LONG).show();
-            mAuth.signOut();
-            act.startActivity(new Intent(act, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-        }
-        }
-
-    @Override
-    public void onFailure(int responseCode, String responseMessage) {
-
-    }
 
 
     public interface RespondsListener {
@@ -71,43 +40,15 @@ public class sendApiRequest implements testIsExitsUser.RespondsListener{
         sendRequest();
     }
 
-
-
-
-    public sendApiRequest(Activity act, RespondsListener respondsListener, String type, String netnetworkUrl, String message) {
-        this.act = act;
-        this.respondsListener = respondsListener;
-        this.type = type;
-        this.networkurl = netnetworkUrl;
-        // display a progress dialog for good user experiance
-        progressDialog = new ProgressDialog(act);
-        progressDialog.setMessage(message);
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-        sendRequest();
-    }
-
     private void sendRequest() {
 
         Log.d("url", "url" + networkurl);
         StringRequest jsObjRequest = new StringRequest(Request.Method.GET,networkurl,
                 response -> {
-                    // dismiss the progress dialog after receiving Constants from API
-                    if(progressDialog != null) progressDialog.dismiss();
-
-                    sendApiRequest.this.type = type;
-                    sendApiRequest.this.responseString = response;
-
-                    mAuth = FirebaseAuth.getInstance();
-                    String mail = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
-                    String pass = mAuth.getUid();
-
-
-                    new testIsExitsUser(act, sendApiRequest.this, mail, pass);
+            respondsListener.onSuccess(response, this.type);
                 },
                 error -> {
                     // dismiss the progress dialog after receiving Constants from API
-                    if(progressDialog != null) progressDialog.dismiss();
                         NetworkResponse response = error.networkResponse;
                         if (response != null) {
                             int code = response.statusCode;
