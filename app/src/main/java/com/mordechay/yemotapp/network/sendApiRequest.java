103dd99031fa46edee527cbeef5fb4c3ae129f84
Mordechay <m8572998@gmail.com>
2023-02-22 23:05:35 +0200

    Added support for the 'No Internet' screen

--- app/src/main/java/com/mordechay/yemotapp/network/sendApiRequest.java
+++ app/src/main/java/com/mordechay/yemotapp/network/sendApiRequest.java
@@ -2,7 +2,6 @@ package com.mordechay.yemotapp.network;
 
 import android.app.Activity;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
@@ -10,9 +9,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.mordechay.yemotapp.data.DataTransfer;
+import com.mordechay.yemotapp.ui.layoutViews.ErrorNoInternetView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class sendApiRequest{
     private final String  type;
@@ -20,6 +23,7 @@ public class sendApiRequest{
     private final RespondsListener respondsListener;
 
     private final String networkurl;
+    private final ErrorNoInternetView errorNoInternetView;
     private JSONObject jsonObject = null;
 
 
@@ -37,54 +41,71 @@ public class sendApiRequest{
         this.respondsListener = respondsListener;
         this.type = type;
         this.networkurl = netnetworkUrl;
+        this.errorNoInternetView = new ErrorNoInternetView(act, respondsListener);
         sendRequest();
     }
 
     private void sendRequest() {
-
-        Log.d("url", "url" + networkurl);
-        StringRequest jsObjRequest = new StringRequest(Request.Method.GET,networkurl,
-                response -> {
-            respondsListener.onSuccess(response, this.type);
-                },
-                error -> {
-                    // dismiss the progress dialog after receiving Constants from API
+        if (!errorNoInternetView.isShowing()) {
+            Log.d("url", "url" + networkurl);
+            StringRequest jsObjRequest = new StringRequest(Request.Method.GET, networkurl,
+                    response -> {
+                        respondsListener.onSuccess(response, this.type);
+                    },
+                    error -> {
+                        // dismiss the progress dialog after receiving Constants from API
                         NetworkResponse response = error.networkResponse;
                         if (response != null) {
                             int code = response.statusCode;
 
                             String errorMsg = new String(response.data);
-                                try {
-                                    jsonObject = new JSONObject(errorMsg);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-
-                                if(jsonObject != null){
-                            if (!jsonObject.isNull("message")) {
-                                String msg = jsonObject.optString("message");
-                                respondsListener.onFailure(code, msg);
+                            try {
+                                jsonObject = new JSONObject(errorMsg);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
+
+
+                            if (jsonObject != null) {
+                                if (!jsonObject.isNull("message")) {
+                                    String msg = jsonObject.optString("message");
+                                    respondsListener.onFailure(code, msg);
+                                }
                             }
 
 
                         } else {
                             String errorMsg = error.getMessage();
-                            Toast.makeText(act, "אנא בדקו את החיבור לאינטרנט ונסו שוב", Toast.LENGTH_LONG).show();
-                                respondsListener.onFailure(0, errorMsg);
-                            }
-                });
+                            respondsListener.onFailure(0, errorMsg);
+                            saveUrl();
+                        }
+                        if(!errorNoInternetView.isShowing()) {
+                            errorNoInternetView.show();
+                        }
+                    });
 
-try{
-        jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(
-                1000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        RequestQueue requestqueue = Volley.newRequestQueue(act);
-        requestqueue.add(jsObjRequest);
-}catch (NullPointerException e){
-    e.printStackTrace();
-}
+            try {
+                jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(
+                        1000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                RequestQueue requestqueue = Volley.newRequestQueue(act);
+                requestqueue.add(jsObjRequest);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }else{
+            saveUrl();
+        }
+    }
+
+    private void saveUrl(){
+        ArrayList<String> listUrl = DataTransfer.getListUrl();
+        listUrl.add(this.networkurl);
+        DataTransfer.setListUrl(listUrl);
+        ArrayList<String> listType = DataTransfer.getListType();
+        listType.add(this.type);
+        DataTransfer.setListType(listType);
+        Log.e("tag" + listUrl.size(), this.networkurl);
     }
 }
